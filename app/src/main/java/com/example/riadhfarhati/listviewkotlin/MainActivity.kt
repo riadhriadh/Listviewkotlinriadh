83150cafdd6f92c928d335cdcd3dd49c204c82c4
riadhfarhati <farahtired@gmail.com>
2017-05-21 16:41:20 +0200

    Initial commit

---
+++ app/src/main/java/com/example/riadhfarhati/listviewkotlin/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.example.riadhfarhati.listviewkotlin
+
+import android.content.Context
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+
+import android.widget.ListView
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val lv = findViewById(R.id.list) as ListView
+        lv.adapter = ListExampleAdapter(this)
+
+
+    }
+
+    private class ListExampleAdapter(context: Context) : BaseAdapter() {
+        internal var sList = arrayOf("riadh", "aymen", "ahmed", "kalid", "khairi", "hossem",
+                "wissem", "mohamed", "sayf", "amir", "sami", "karim")
+        private val mInflator: LayoutInflater
+
+        init {
+            this.mInflator = LayoutInflater.from(context)
+        }
+
+        override fun getCount(): Int {
+            return sList.size
+        }
+
+        override fun getItem(position: Int): Any {
+            return sList[position]
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View? {
+            val view: View?
+            val vh: ListRowHolder
+            if (convertView == null) {
+                view = this.mInflator.inflate(R.layout.uneitem, parent, false)
+                vh = ListRowHolder(view)
+                view.tag = vh
+            } else {
+                view = convertView
+                vh = view.tag as ListRowHolder
+            }
+
+            vh.label.text = sList[position]
+            return view
+        }
+    }
+
+    private class ListRowHolder(row: View?) {
+        public val label: TextView
+
+
+        init {
+            this.label = row?.findViewById(R.id.textView) as TextView
+        }
+    }
+
+
+
+
+
+
+
+
+
+}
